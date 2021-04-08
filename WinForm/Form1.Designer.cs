9c76bb15db1b1f056a859b19fe675aa01a27475b
cmaradiaga-coa <79603629+cmaradiaga-coa@users.noreply.github.com>
2021-04-07 23:14:10 -0600

    Add project files.

---
+++ WinForm/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.picLoadingGif = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picLoadingGif)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(53, 103);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(170, 41);
+            this.btnIniciar.TabIndex = 0;
+            this.btnIniciar.Text = "Iniciar Proceso";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // picLoadingGif
+            // 
+            this.picLoadingGif.Image = ((System.Drawing.Image)(resources.GetObject("picLoadingGif.Image")));
+            this.picLoadingGif.Location = new System.Drawing.Point(252, 103);
+            this.picLoadingGif.Name = "picLoadingGif";
+            this.picLoadingGif.Size = new System.Drawing.Size(111, 65);
+            this.picLoadingGif.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picLoadingGif.TabIndex = 1;
+            this.picLoadingGif.TabStop = false;
+            this.picLoadingGif.Visible = false;
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picLoadingGif);
+            this.Controls.Add(this.btnIniciar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picLoadingGif)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.PictureBox picLoadingGif;
+    }
+}
+
---
+++ WinForm/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnIniciar_Click(object sender, EventArgs e)
+        {
+            // Operacion Sincrona. Esta operacion Bloquea el Hilo de ejecucion durante 5 segundos, por lo que Nada mas se puede usar hasta que termiane.
+            // Thread.Sleep(5000);
+
+            // Operacion Asincrona. No Bloquea el Hilo de ejecucion y se pueden realizar otras tareas mientras tanto. 
+            picLoadingGif.Visible = true;
+            await Task.Delay(TimeSpan.FromSeconds(5));
+
+            picLoadingGif.Visible = false;
+
+            //.. Cuando termina, siguen el resto de operaciones, pero no Bloquea la ejecucion.
+        }
+
+      
+    }
+}
